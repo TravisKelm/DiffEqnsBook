b1c56b9a83a28ec9218af5458d5d213e3c0ef7a5
Travis R. Kelm <tkelm@mail.fresnostate.edu>
2026-05-19 19:43:11 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Chapter 1",
-  "body": " Chapter 1  Differential Equations   An equation relating an unknown function and one or more of its derivatives is called a differential equation }(or DE for short).  A DE in one independent variable is called an ordinary differential equation (short, ODE). A DE in several independent variables is called a partial differential equation (or PDE).  The order of a differential equation is the order of the highest derivative of the unknown function in the equation.     Some Examples     is a first-order ODE in independent variable .   is a first-order PDE in two independent variables, and .   is a third-order ODE in independent variable .   is a second-order PDE in three independent variables , and (this is a two-dimensional heat equation ).      Note  The prior examples demonstrate various notations for derivatives.   Recall the following. Let be a function of , so .  For first derivatives, the notations and are equivalent.  For higher order derivatives, the notations and are equivalent.    Let be a function of and . So . Then we may represent the partial derivatives by, for example: or, equivalently    This book only deals with ODEs  For the remainder of this textbook, we will only consider ordinary differential equations.   The most general form of an -order ordinary differential equation with independent variable and unknown function (or dependent variable) is where is a function of the variables.  An -order differential equation solved relative to the highest derivative (which may not be possible) is said to be in standard (or normal ) form: where is a function of the variables .  Thus, in Example the prior examples, is in standard form and is not.   Solution of a Differential Equation   A function is called a solution solution of differential equation on an interval if it is times differentiable on and for all .  The function is also said to satisfy satisfy differential equation on the interval .      For any , the function is a solution of the DE on the interval     "
+  "body": " Chapter 1  Differential Equations   An equation relating an unknown function and one or more of its derivatives is called a differential equation (or DE for short).  A DE in one independent variable is called an ordinary differential equation (short, ODE). A DE in several independent variables is called a partial differential equation (or PDE).  The order of a differential equation is the order of the highest derivative of the unknown function in the equation.     Examples of Differential Equations     is a first-order ODE in independent variable .   is a first-order PDE in two independent variables, and .   is a third-order ODE in independent variable .   is a second-order PDE in three independent variables , and (this is a two-dimensional heat equation ).      Notations for Derivatives  The prior examples demonstrate various notations for derivatives.   Recall the following. Let be a function of , so .  For first derivatives, the notations and are equivalent.  For higher order derivatives, the notations and are equivalent.    Let be a function of and . So . Then we may represent the partial derivatives by, for example: or, equivalently    This book only deals with ODEs  For the remainder of this textbook, we will only consider ordinary differential equations.   The most general form of an -order ordinary differential equation with independent variable and unknown function (or dependent variable) is where is a function of the variables.  An -order differential equation solved relative to the highest derivative (which may not be possible) is said to be in standard (or normal ) form: where is a function of the variables .  Thus, in the prior examples, is in standard form and is not.   Solution of a Differential Equation   A function is called a solution solution of differential equation on an interval if it is times differentiable on and for all .  The function is also said to satisfy satisfy differential equation on the interval .      For any , the function is a solution of the DE on the interval     "
 },
 {
   "id": "def-DifferentialEquation",
@@ -25,7 +25,7 @@ var ptx_lunr_docs = [
   "type": "Definition",
   "number": "1.1.1",
   "title": "",
-  "body": "Differential Equations   An equation relating an unknown function and one or more of its derivatives is called a differential equation }(or DE for short).  A DE in one independent variable is called an ordinary differential equation (short, ODE). A DE in several independent variables is called a partial differential equation (or PDE).  The order of a differential equation is the order of the highest derivative of the unknown function in the equation.   "
+  "body": "Differential Equations   An equation relating an unknown function and one or more of its derivatives is called a differential equation (or DE for short).  A DE in one independent variable is called an ordinary differential equation (short, ODE). A DE in several independent variables is called a partial differential equation (or PDE).  The order of a differential equation is the order of the highest derivative of the unknown function in the equation.   "
 },
 {
   "id": "example-DEs",
@@ -33,8 +33,8 @@ var ptx_lunr_docs = [
   "url": "sec-chpt1.html#example-DEs",
   "type": "Example",
   "number": "1.1.2",
-  "title": "Some Examples.",
-  "body": " Some Examples     is a first-order ODE in independent variable .   is a first-order PDE in two independent variables, and .   is a third-order ODE in independent variable .   is a second-order PDE in three independent variables , and (this is a two-dimensional heat equation ).    "
+  "title": "Examples of Differential Equations.",
+  "body": " Examples of Differential Equations     is a first-order ODE in independent variable .   is a first-order PDE in two independent variables, and .   is a third-order ODE in independent variable .   is a second-order PDE in three independent variables , and (this is a two-dimensional heat equation ).    "
 },
 {
   "id": "note-deriv-notations",
@@ -42,8 +42,8 @@ var ptx_lunr_docs = [
   "url": "sec-chpt1.html#note-deriv-notations",
   "type": "Note",
   "number": "1.1.3",
-  "title": "Note.",
-  "body": " Note  The prior examples demonstrate various notations for derivatives.  "
+  "title": "Notations for Derivatives.",
+  "body": " Notations for Derivatives  The prior examples demonstrate various notations for derivatives.  "
 },
 {
   "id": "warning-only-ODEs",
@@ -88,7 +88,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.2",
   "title": "Exercises for Chapter 1",
-  "body": " Exercises for Chapter 1   Verify the solution in Example .  Give an example of a differential equation whose solution is on .  Give an example of a differential equation and its solution.   "
+  "body": " Exercises for Chapter 1   Verify the solution in Example .  Give an example of a differential equation whose solution is .  Give an example of a differential equation and its solution.   "
 },
 {
   "id": "backmatter-2",
